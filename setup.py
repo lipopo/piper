c500507347499b28c90a4f85b1d486c9cf464610
lizhiping <lizhiping11@huawei.com>
2020-08-25 17:37:59 +0800

    Setup requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+import os
 from setuptools import setup, find_packages
+
+
+# Setup require packages
+_require = []
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f:
+    _require = [pack[:-1] for pack in f.readlines()]
 
 
 setup(
     name="Piper",
     version="0.0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=_require
 )
