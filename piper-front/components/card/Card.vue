fd1b09065a4311f1de71638ea081d73d8fdb7a56
lizhiping <lizhiping11@huawei.com>
2020-09-01 18:31:36 +0800

    Setup Base Card

---
+++ piper-front/components/card/Card.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+div.card
+  slot
+</template>
+
+<style lang='stylus'>
+@import '../../assets/core/depth.styl'
+@import '../../assets/core/layout.styl'
+
+.card
+  @extend .p-sm-5
+  @extend .d1-c0
+
+  &:hover
+    @extend .d2-c0
+
+</style>
