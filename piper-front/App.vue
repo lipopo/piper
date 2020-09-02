4d814d2531e8c32e3f9a11c4dff42ccfd269d860
lipo <lipo8081@gmail.com>
2020-09-03 04:58:08 +0800

    Register App Event And Flash Event

--- piper-front/App.vue
+++ piper-front/App.vue
@@ -2,51 +2,106 @@
 div.layout
   div.nav
     Navigator(title='Piper' :version='version')
+      Icon(
+        v-if='loading'
+        iconName='refresh'
+        fontColor='#fff'
+        :spin='true'
+      )
   div.content
     Drawer.drawer(:open='drawer_open')
       div.drawer-header.p-sm-5(slot='header' @click='open_drawer')
-        span.flow-icon o
+        div.inline-block(:class='drawer_open ? "" : "r-Z-180"') -)
       Menu(
         :fontColor='"#fff"'
         :menuBgColor='Color.vio.darkest'
         :itemActiveColor='Color.vio.default'
         :itemHoverColor='Color.vio.dark'
         :items='menu_item'
+        @activeItem='activePage'
       )
-    div.com-content.p-sm-5
-      component(:is='content')
+    div.com-content.p-sm-5(
+      :style="{width: `calc(100% - ${!drawer_open ? '2rem' : '10rem'})`}")
+      div.flash-area
+        Flash.g-sm-5-b(
+          v-for='item, idx in flash_list'
+          :type='item.type'
+          :iconName='item.iconName'
+          :content='item.content'
+          @close='(idx) => close_flash(idx)'
+        )
+      keep-alive
+        component(:is='content')
 </template>
 
 <script>
 import { version } from './lib/index.coffee';
+import AppCls from './app.coffee';
+
 import Color from './assets/core/color.js';
+import Icon from './assets/icon/Icon.vue';
+
 import Drawer from './components/drawer/Drawer.vue';
+import FlashE from './components/flash/flash.coffee';
+import Flash from './components/flash/Flash.vue';
 import Menu from './components/menu/Menu.vue';
 import Navigator from './components/navigator/Navigator.vue';
+
+import { 
+  FlowBuilderPage,
+  ConfigPage } from './page';
 // import ElementFlow from './components/element/ElementFlow.vue';
 
 export default {
   components: {
     Navigator,
     Drawer,
-    Menu
+    Menu,
+    Icon,
+    FlowBuilderPage,
+    ConfigPage,
+    Flash
     // ElementFlow
   },
   data() {
     return {
       version: version,
       drawer_open: false,
-      content: null,
+      content: 'flow-builder-page',
       base_menu_item: [
-        {name: 'Flow', icon:'flow-icon'},
-        {name: 'Config', icon: 'config-icon'}
+        {name: 'Flow', icon:'random', icls: 'r-Z-90', page: 'flow-builder-page'},
+        {name: 'Config', icon: 'cog', page: 'config-page'}
       ],
-      Color: Color
+      Color: Color,
+      flash_list: [],
+      loading: false,
+      app: new AppCls()
     }
+  },
+  mounted: function() {
+    let flash = new FlashE();
+    flash.onflash(this.push_flash);
+    this.app.onloading(this.set_loading);
+    this.app.onloaded(this.set_loaded);
   },
   methods: {
     open_drawer: function() {
       this.drawer_open = !this.drawer_open;
+    },
+    activePage: function(pageIdx) {
+      this.content = this.base_menu_item[pageIdx].page
+    },
+    push_flash: function(item) {
+      this.flash_list.push(item);
+    },
+    close_flash: function(idx) {
+      this.flash_list.splice(idx, 1);
+    },
+    set_loading: function() {
+      this.loading = true;
+    },
+    set_loaded: function() {
+      this.loading = false;
     }
   },
   computed: {
@@ -56,7 +111,8 @@ export default {
       }else {
         return this.base_menu_item.map( (item) => {
           return {
-            name: `<span class='${item.icon}'>${item.name[0]}</span>`
+            icon: item.icon,
+            icls: item.icls
           };
         });
       }
@@ -76,6 +132,7 @@ export default {
   .content
     position relative
     flex-grow 1
+    min-height 100rem
     .drawer
       @extend .bg-vio-darkest
       color #fff
@@ -85,5 +142,7 @@ export default {
         cursor pointer
         &:hover
           @extend .bg-vio-dark
+    .com-content
+      width calc(100% - 2rem)
 
 </style>
